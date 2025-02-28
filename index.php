344dd95e7362e9781bb6c8fef4451915899af943
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2025-02-28 09:14:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<?php include('./partials/header.php') ?>
+
+<div class="container py-5">
+    <h1>All Proucts</h1>
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/apple.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/banana.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/beans.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/bread.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/cheese.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/chicken.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/coke.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/egg.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+            <div class="card">
+                <img src="./img/honey.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <span class="category-text">beverage</span>
+                    </div>
+                    <div class="d-flex align-item-center justify-content-between">
+                        <h5 class="card-title">₦1,000</h5>
+                        <span class="text-danger">₦<del>1,200</del></span>
+                    </div>
+                    <p class="card-text">Product text, this is just a dummy text, a better one would come</p>
+                    <a href="product.php" class="btn btn-primary w-100">View Product</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('./partials/footer.php') ?>
---
+++ product.php
@@ -0,0 +1,26 @@
+<?php include('./partials/header.php') ?>
+
+<div class="container py-5">
+    <h1 class="text-start">Prouct Detail</h1>
+    <div class="d-sm-flex d-block">
+        <div style="flex: 1">
+            <img src="./img/apple.jpeg" alt="" class="w-100">
+        </div>
+        <div style="flex: 1">
+            <h2>Apple (20grams)</h2>
+            <p>Category: Beverages</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, soluta odio. Dolore quaerat ab quos sit maiores ratione fugiat, eaque officia et eius praesentium, repellat nulla saepe. Consequatur, itaque officia!</p>
+            <div class="h3">
+                <a href="">
+                    <i class="bi bi-file-plus-fill"></i>
+                </a>
+                <span>3</span>
+                <a href="">
+                    <i class="bi bi-file-minus-fill"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('./partials/footer.php') ?>
